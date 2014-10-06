2507f12ffacf509bb83c795e36c7e16bdc51481e
Nick Bibikov <n.bibikov@me.com>
2014-10-06 11:59:43 +0300

    Initial Commit

---
+++ Lesson19-View/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Lesson19-View
+//
+//  Created by Nick Bibikov on 10/6/14.
+//  Copyright (c) 2014 NickBibikov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Lesson19-View/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Lesson19-View
+//
+//  Created by Nick Bibikov on 10/6/14.
+//  Copyright (c) 2014 NickBibikov. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
